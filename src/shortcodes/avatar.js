c75bce027b9445be2b6ef65a254a3ef45b6aa56d
nd55 <45429767+nd55@users.noreply.github.com>
2022-01-04 17:34:01 +0100

    local image manipulation whoooooop!

--- src/shortcodes/avatar.js
+++ src/shortcodes/avatar.js
@@ -1,3 +1,43 @@
+const Cache = require("@11ty/eleventy-cache-assets");
+const sharp = require("sharp");
+const fs = require("fs");
+const fsExtra = require("fs-extra");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const randomNumber = () =>
+  Date.now() + "-" + Math.random().toFixed(6).substring(2);
+
+const dir = "./dist/images/generated";
+
+const getLocalImagePath = (url) => {
+  const fileName = `${randomNumber()}.webp`;
+  const finalSize = 400;
+
+  const generateImage = async () => {
+    const imageBuffer = await Cache(url, {
+      duration: "1d",
+      type: "buffer",
+    });
+
+    sharp(imageBuffer)
+      .resize({ width: finalSize, height: finalSize, fit: "cover" })
+      .toBuffer((err, data, info) => {
+        if (err) {
+          return;
+        }
+
+        fs.writeFile(`${dir}/${fileName}`, data, (err) => {});
+      });
+  };
+
+  generateImage();
+
+  return `/images/generated/${fileName}`;
+};
+
+//
+
 module.exports = (avatar) => {
   const avatarCounter = Math.floor(Math.random() * 5 + 1);
   let source = "/images/avatar-0" + avatarCounter + ".webp";
@@ -5,6 +45,7 @@ module.exports = (avatar) => {
   let size = "";
   let classes = "";
   let alt = "alt = ''";
+
   if (avatar.src) {
     source = avatar.src;
     if (source.includes("https://www.gravatar.com")) {
@@ -15,16 +56,28 @@ module.exports = (avatar) => {
     if (avatar.name) {
       alt = "alt = '" + avatar.name + "'";
     }
+
+    // if (isProduction)
+    if (true) {
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+
+      fsExtra.emptyDirSync(dir);
+
+      source = getLocalImagePath(source);
+    }
   } else {
     hidden = "aria-hidden='true'";
   }
+
   if (avatar.size) {
     size = "width='" + avatar.size + "' height='" + avatar.size + "'";
   }
+
   if (avatar.classlist) {
     classes = " " + avatar.classlist;
   }
 
-  return `<img loading="lazy" decoding="async" class="avatar${classes}" src="${source}" ${alt} ${hidden} ${size}>
-  `;
+  return `<img loading="lazy" decoding="async" class="avatar${classes}" src="${source}" ${alt} ${hidden} ${size}>`;
 };
