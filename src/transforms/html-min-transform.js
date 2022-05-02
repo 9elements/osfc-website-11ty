23acf41f1c938561dc2f7fa0262881c06afbe058
Nils Binder <nils.binder@icloud.com>
2022-05-02 14:40:49 +0200

    Update html-min-transform.js

--- src/transforms/html-min-transform.js
+++ src/transforms/html-min-transform.js
@@ -3,11 +3,11 @@ const htmlmin = require("html-minifier");
 module.exports = (value, outputPath) => {
   if (outputPath && outputPath.endsWith(".html")) {
     return htmlmin.minify(value, {
-      //useShortDoctype: true,
-      //removeComments: true,
-      //collapseWhitespace: true,
-      //minifyCSS: true,
-      //minifyJs: true,
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJs: true,
     });
   }
 
