ad33bdb29849a4391d39ab88ccb4304a4ad980c0
Nils Binder <nils.binder@icloud.com>
2022-09-07 13:05:50 +0200

    add year identifier

--- src/2022/2022.11tydata.js
+++ src/2022/2022.11tydata.js
@@ -18,6 +18,18 @@ module.exports = async () => {
       }
     );
 
+    const speakers2022 = await Cache(
+      `https://pretalx.com/api/events/osfc2022/speakers/?format=json&limit=200`,
+      {
+        duration: "1m", // 1 day
+        type: "json"
+      }
+    );
+
+    //console.log(speakers2022.count);
+
+    speakers2022.results.sort((a, b) => (a.name > b.name ? 1 : -1));
+
     const talks = await Cache(
       //`https://pretalx.com/api/events/osfc2021/talks/?limit=200`,
       `https://pretalx.com/api/events/osfc2022/submissions/?format=json&limit=200`,
@@ -37,19 +49,6 @@ module.exports = async () => {
       // (talk) => talk.state === "confirmed" && talk.is_featured
     );
 
-    const speakers = await Cache(
-      `https://talks.osfc.io/api/events/osfc2022/speakers/?limit=200`,
-      {
-        duration: "1d", // 1 day
-        type: "json",
-        fetchOptions: {
-          headers: {
-            Authorization: "Token 1bfe4598ca6e29bb43e1e09510915432196d76c4"
-          }
-        }
-      }
-    );
-
     const breaks = await Cache(
       `https://pretalx.com/api/events/osfc2022/schedules/latest/?format=json`,
       {
@@ -62,8 +61,6 @@ module.exports = async () => {
         }
       }
     );
-
-    speakers.results.sort((a, b) => (a.name > b.name ? 1 : -1));
 
     // const videos = await Cache(`https://cfp.osfc.io/api/events/osfc2021/p/vimeo/`, {
     //   duration: "1d", // 1 day
@@ -83,7 +80,7 @@ module.exports = async () => {
     return {
       schedule: schedule.schedule.conference,
       talks: confirmedTalks,
-      speakers: speakers.results,
+      speakers: speakers2022.results,
       breaks: breaks.breaks
     };
   } catch (error) {
