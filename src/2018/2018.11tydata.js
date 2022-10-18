f74c8b1859aeae56cfc7d54848d7334f2e68bd92
Denise Brachwitz <denise.brachwitz@9elements.com>
2022-10-18 16:38:24 +0300

    Fix 2018 and 2019 video settings (#33)

--- src/2018/2018.11tydata.js
+++ src/2018/2018.11tydata.js
@@ -52,32 +52,33 @@ module.exports = async () => {
 
     speakers.results.sort((a, b) => (a.name > b.name ? 1 : -1));
 
-    // const videos = await Cache(
-    //   `https://cfp.osfc.io/api/events/osfc2019/p/vimeo/`,
-    //   {
-    //     duration: "1d", // 1 day
-    //     type: "json",
-    //     fetchOptions: {
-    //       headers: {
-    //         Authorization: "Token 1bfe4598ca6e29bb43e1e09510915432196d76c4",
-    //       },
-    //     },
-    //   }
-    // );
+    const videos = await Cache(
+      `https://cfp.osfc.io/api/events/osfc2018/p/vimeo/`,
+      {
+        duration: "1d", // 1 day
+        type: "json",
+        fetchOptions: {
+          headers: {
+            Authorization: "Token 1bfe4598ca6e29bb43e1e09510915432196d76c4",
+          },
+        },
+      }
+    );
 
-    // const newVideos = videos.results.map((video) => {
-    //   return {
-    //     ...video,
-    //     vimeo_id: video.vimeo_link.substring(
-    //       video.vimeo_link.lastIndexOf("/") + 1
-    //     ),
-    //   };
-    // });
+    const newVideos = videos.results.map((video) => {
+      return {
+        ...video,
+        vimeo_id: video.vimeo_link.substring(
+          video.vimeo_link.lastIndexOf("/") + 1
+        ),
+      };
+    });
 
     return {
       schedule: schedule.schedule.conference,
       talks: confirmedTalks,
       speakers: speakers.results,
+      videos: newVideos
     };
   } catch (error) {
     console.log(error);
