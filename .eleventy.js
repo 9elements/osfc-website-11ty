67ed623917389e9ceb71725403b5e46cdc78a839
nd55 <45429767+nd55@users.noreply.github.com>
2025-09-08 12:31:42 +0200

    update dependencies and remove pnpm-lock

--- .eleventy.js
+++ .eleventy.js
@@ -97,7 +97,6 @@ export default function EleventyConfig(config) {
 
   // Only minify HTML if we are in production because it slows builds _right_ down
   if (isProduction) {
-    config.addTransform("htmlmin", htmlMinify);
     config.addTransform("purgeCSS", purgeCSS);
   }
 
